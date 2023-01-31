294833dffcc5b488f60c7864f718405f87629287
Nick Selivanov <edu.selivanov@ya.ru>
2023-01-31 16:47:19 +0300

    add Navbar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,20 +1,16 @@
-import React, {Suspense} from 'react';
-import {Route, Routes} from "react-router-dom";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import {routerConfig} from "shared/routerConfig/routerConfig";
+import { routerConfig } from "shared/config/routerConfig/routerConfig";
 
 const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(routerConfig).map(({element, path}) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={element}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+  <Suspense fallback={<div>Loading...</div>}>
+    <Routes>
+      {Object.values(routerConfig).map(({ element, path }) => (
+        <Route key={path} path={path} element={element} />
+      ))}
+    </Routes>
+  </Suspense>
 );
 
 export default AppRouter;
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,27 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+
+import cls from "./Navbar.module.scss";
+
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
+  return (
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <div className={cls.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.mainLink}
+          to={"/"}
+        >
+          Главная
+        </AppLink>
+        <AppLink theme={AppLinkTheme.PRIMARY} to={"/about"}>
+          О нас
+        </AppLink>
+      </div>
+    </div>
+  );
+};
