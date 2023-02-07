363c8800605a4cb155b6b1b3ec6603241b5a78c1
Nick Selivanov <edu.selivanov@ya.ru>
2023-02-07 17:05:40 +0300

    added custom button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import { classNames } from "shared/lib/classNames/classNames";
+import { ButtonHTMLAttributes, FC } from "react";
+import cls from "./Button.module.scss";
+
+export enum ThemeButton {
+  CLEAR = "clear",
+}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  theme?: ThemeButton;
+}
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { className, children, theme, ...otherProps } = props;
+  return <div className={classNames(cls.button, {}, [className])}></div>;
+};
