f09405a5856ea92b72d8c7e317c0855872afe2ab
Nick Selivanov <edu.selivanov@ya.ru>
2023-03-10 15:31:46 +0300

    add async reducer for user auth

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,9 +1,11 @@
 import { StoreProvider } from './ui/StoreProvider';
 import { createReduxStore } from './config/store';
-import type { StateSchema } from './config/StateSchema';
+import type { StateSchema, ReduxStoreWithManager, StateSchemaKey } from './config/StateSchema';
 
 export {
     StoreProvider,
     createReduxStore,
     StateSchema,
+    ReduxStoreWithManager,
+    StateSchemaKey,
 };
